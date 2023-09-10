6459b5904140bd1db5b5337e0e17b20594c8f548
myspaceapp <myspaceappservice@gmail.com>
2023-09-10 16:09:39 +0530

    adding proj

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+const routes: Routes = [
+    { path : "avis",
+      component : HomepageComponent
+    },
+    {
+      path: '',
+      redirectTo: '/avis',
+      pathMatch: 'full',
+    },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
