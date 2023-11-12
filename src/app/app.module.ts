96a44dfc5dba4f8541224a1bed596e0a5461aaca
myspaceapp <myspaceappservice@gmail.com>
2023-11-12 17:54:18 +0530

    general update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AvisComponent } from './avis/avis.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    AvisComponent,
+    SidebarComponent    
   ],
   imports: [
     BrowserModule,
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+    HomePageComponent() {
+      
+    }
 }
